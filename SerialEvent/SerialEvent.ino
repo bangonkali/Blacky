31250d714a56afc115f7de15b4d9187f7fcbeef5
Bangon Kali <bangonkali@istoryahi.me>
2013-02-20 21:49:11 +0800

    Added Arduino component used for debugging serial i/o from pic.

---
+++ SerialEvent/SerialEvent.ino
@@ -0,0 +1,22 @@
+void setup() {
+  // initialize serial:
+  Serial.begin(9600);
+  Serial1.begin(115200);
+}
+
+char prevHp = 'z';
+
+void loop() {
+  // print the string when a newline arrives:
+  while (Serial1.available() ) {
+    char h = Serial1.read();
+    if (prevHp != h) {
+      Serial.print(h);
+      prevHp = h;
+    } else {
+      prevHp = h;
+    }
+  }
+}
+
+
